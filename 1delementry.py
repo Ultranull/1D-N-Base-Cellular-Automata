b99ba15a3af9b843cee05fe9ba6a48678b64eaad
Josh Rich <boundtodoom@gmail.com>
2019-11-19 00:01:02 -0500

    its better now

--- 1delementry.py
+++ 1delementry.py
@@ -1,6 +1,29 @@
+
 import random
+import argparse
+from PIL import Image
 
 numerals = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def inter(a:float,b:float,t:float):
+    return int(t*b+(1-t)*a)
+
+def colorToHex(c):
+    return (c[2]<<8*2)+(c[1]<<8)+c[0]
+
+def colorInter(a,b,t):
+    return (inter(a[0],b[0],t),inter(a[1],b[1],t),inter(a[2],b[2],t))
+
+def colorHist(a,b,c,t):
+    if t < 1/2.0:
+        return colorInter(a,b,t*2.0)
+    return colorInter(b,c,(t-1/2.0)*2)
+
+def colorHist2(lst,t):
+    size = len(lst)
+    for i in range(0,size-1):
+        if t <= (i+1)/(size-1):
+            return colorInter(lst[i],lst[i+1],(t-i/float(size-1))*(size-1))
+
 
 
 def baseN(n, b):
@@ -22,52 +45,76 @@ def baseNtodec(n, b):
 
 def getstate(last, row, base):
     parent = []
-    for loc in [-1, 0, 1]:
+    c = neighbors-int(neighbors/2)-1
+    for loc in range(-c,c+1):
         if loc + row >= len(last) or loc + row < 0:
             parent.append(0)
-        else: parent.append(numerals.find(last[row + loc]))
-    out = rule_size - baseNtodec(parent, base)
+        else: parent.append(last[row + loc])
+    bn = baseNtodec(parent, base)
+    out = rule_size - 1 - bn
     return rule[out] 
 
 
 def nextgen(last, base):
-    ans = ""
+    ans = []
     for i in range(len(last)):
-        ans += getstate(last, i, base)
+        ans += [getstate(last, i, base)]
     return ans
 
 
-rule_10 = int(input("Enter rule(as decimal number):"))
-base = int(input("Enter base(as decimal number):"))
+parser = argparse.ArgumentParser()
+parser.add_argument('rule',type = int)
+parser.add_argument('base',type = int)
+parser.add_argument('-c','--colors', nargs='+', type = int, default= [0,0,0xb0,0xf0,0x89,0x13])
+parser.add_argument('-n','--neighbors', type = int, default= 3)
+parser.add_argument('-r','--random_start',action='store_true')
+args = parser.parse_args()
 
-rule_size = (base ** 3) - 1
+customPalette = [(args.colors[i],args.colors[i+1],args.colors[i+2]) for i in range(0,len(args.colors),3)]
+
+rule_10 = args.rule
+base = args.base
+neighbors = args.neighbors
+
+rule = []
+
+rule_size = (base ** (neighbors))
 if rule_10 < 0 :
-    newRule = ''
-    for _ in range(rule_size):
-        newRule += str(random.randint(0, 9))
-    rule_10 = int(newRule)
-
-rule_list = baseN(rule_10, base)
-
-rule = ''
-for n in rule_list:
-    rule += numerals[n]
+    rule = [random.randint(0, base-1) for _ in range(rule_size)]
+else:
+    rule = baseN(rule_10, base)
 
 if (len(rule) != rule_size):
-    pad = ''
-    for _ in range(0, rule_size - len(rule) + 1):
-        pad += '0'
+    pad = [0 for _ in range(0, rule_size - len(rule))]
     rule = pad + rule
 
-dem = (100, 50)
+while len(rule) > rule_size:
+    rule.pop(0)
+
+dem = (700, 350)
 gens = []
-ans = ""
-for _ in range(int(dem[0] / 2)):
-    ans += "0"
-gens += [ans + numerals[base - 1] + ans]
+ans = []
 
-for row in range(1, dem[1]):
-    gens += [nextgen(gens[row - 1], base)]
-for line in gens:
-    print(line.replace("0", " "))
+ans = [0 for _ in range(int(dem[0]/2))]
+gens = ans + [base - 1] + ans
 
+if args.random_start:
+    gens = [random.randint(0, base-1) for i in range(0,dem[0])]
+
+img = Image.new('RGB',dem)
+pixels = img.load()
+for row in range(0, dem[1]):
+    for col in range(0,len(gens)-1):
+        cellIndex = float(gens[col]/float(base-1))
+        color = colorHist2(customPalette,cellIndex)
+        pixels[col,row]= color
+    gens = nextgen(gens, base)
+img.save('test.png')
+
+print(rule)
+
+
+
+
+
+# base 5 24203034231100402041314421044124123423211313212020442000121103242401143234213221044323220312234012114302422423421144442030244231440143121300313044133341102434001334343440042222210
